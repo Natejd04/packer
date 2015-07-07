77490cdd3c496481e117973d65d6c83882bc5433
Nate <Natejd04@gmail.com>
2015-07-06 22:11:19 -0700

    recommend page basically completed

--- app/controllers/recommends_controller.rb
+++ app/controllers/recommends_controller.rb
@@ -1,5 +1,6 @@
 class RecommendsController < ApplicationController
-
+    before_action :all_recommends, only: [:index, :create]
+    
     def index
         @recommends = Recommend.all
         @recommend = Recommend.new
@@ -11,5 +12,23 @@ class RecommendsController < ApplicationController
     
     def new
         @recommend = Recommend.new
+    end
+    
+    def create
+        @recommend = Recommend.create(recommend_params)
+    end
+
+    def update
+       @recommend = Recommend.find(params[:id])
+       @recommend.update(recommend_params)
+    end
+  private
+    
+    def all_recommends
+        @recommends = Recommend.all
+    end
+    
+    def recommend_params
+        params.require(:recommend).permit(:name, :qty)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  resources :hikers
  resources :items
-
+ resources :recommends
  root 'items#index'
+    
 end
